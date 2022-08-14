517ba79e25bd7eb09754af01aed790194f70966d
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-08-14 20:18:26 -0300

    make the backend work with the phonebook frontend (step9)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
@@ -13,6 +14,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 }))
 
 app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }))
+
+app.use(cors())
 
 let persons = [
   {
