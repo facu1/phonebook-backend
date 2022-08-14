65321feea85de98011c4fe8533a7cad8cc4ef565
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-08-14 20:23:03 -0300

    add heroku configs

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/info', (request, response) => {
   response.send(responseMsg)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
